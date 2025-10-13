54ee81d81857cbfc749e82f971ebde3a6d042239
byzanurk <beyzanrtkrk@gmail.com>
2025-10-13 17:53:51 +0300

    add nav bar buttons to pokedex view
    - grid button and sort button
    - button functions will be done later

--- Pokedex/Screens/PokedexView/PokedexViewController.swift
+++ Pokedex/Screens/PokedexView/PokedexViewController.swift
@@ -15,7 +15,45 @@ final class PokedexViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Pokedex"
+        setupNavButtons()
     }
+
+    private func setupNavButtons() {
+            
+        // square.grid.3x3.fill
+        // square.grid.4x3.fill
+        let sortButton = UIBarButtonItem(
+            image: UIImage(systemName: "square.grid.4x3.fill"),
+            style: .plain,
+            target: self,
+            action: #selector(sortButtonTapped)
+        )
+        
+        // line.3.horizontal.decrease
+        let gridButton = UIBarButtonItem(
+            image: UIImage(systemName: "line.3.horizontal.decrease"),
+            style: .plain,
+            target: self,
+            action: #selector(gridButtonTapped)
+        )
+        
+        sortButton.tintColor = .white
+        gridButton.tintColor = .white
+        
+        navigationItem.rightBarButtonItems = [gridButton, sortButton]
+        
+    }
+    
+    @objc private func sortButtonTapped() {
+        // for now
+        print("sort button tapped")
+    }
+    
+    @objc private func gridButtonTapped() {
+        // for now
+        print("grid button tapped")
+    }
+
 
 
 }
