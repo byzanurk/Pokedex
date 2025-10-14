7b549131a56e3cdd57d724e9d88636ca64480b5a
byzanurk <beyzanrtkrk@gmail.com>
2025-10-14 03:07:34 +0300

    add dark theme on all app

--- Pokedex/Core/App/SceneDelegate.swift
+++ Pokedex/Core/App/SceneDelegate.swift
@@ -14,13 +14,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-
+        
         let coordinator = Coordinator()
         let tabBar = coordinator.start()
 
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = tabBar
         window?.makeKeyAndVisible()
+        
+        if #available(iOS 13.0, *) {
+            window?.overrideUserInterfaceStyle = .dark
+        }
         
         self.coordinator = coordinator
     }
