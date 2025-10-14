47341201e9597e9d2b797a89afd73c650a57ba8b
byzanurk <beyzanrtkrk@gmail.com>
2025-10-14 03:06:57 +0300

    add pokemon cell

---
+++ Pokedex/Views/PokemonCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonCollectionViewCell.swift
+//  Pokedex
+//
+//  Created by Beyza Nur Tekerek on 13.10.2025.
+//
+
+import UIKit
+
+class PokemonCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
